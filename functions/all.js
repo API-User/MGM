e492e4d55bee1151c291bfa2e2daedb04a80c75f
API-USer <playertoy@gmx.de>
2016-07-24 19:49:32 +0200

    init

---
+++ functions/all.js
@@ -0,0 +1,22 @@
+function ajax_post(url, vars, callback) {
+	var h = XMLHttpRequest();
+  h.open('POST', url, true);
+  h.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  h.onreadystatechange = function() {
+  	if(h.readyState == 4 && h.status == 200) {
+  		callback(h.responseText);
+  	}
+  }
+  h.send(vars);
+}
+
+function is_mobile() {
+	if(document.width <= 768) {
+		return true;
+	} else return false;
+}
+
+function createPopup(title, url, width, height) {
+		window.open(url, title, 'width='+width+',height='+height);
+}
+
